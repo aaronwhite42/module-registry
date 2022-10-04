3528404bbfbc4c17289c20777791562bc72aaca7
Aaron <argent78@me.com>
2022-10-04 12:01:46 +0800

    First pass implementation of a simple Module Registry

---
+++ ModuleRegistry/ModuleRegistry.Api/Services/IModuleRepository.cs
@@ -0,0 +1,10 @@
+﻿using ModuleRegistry.Api.Models;
+
+namespace ModuleRegistry.Api.Services;
+
+public interface IModuleRepository
+{
+    IEnumerable<Module> ListModules(); //should this just be type string?
+    IEnumerable<string> ListModuleVersions(string moduleName);
+    Uri GetModuleVersionUri(string name, string version);
+}
